e5ef213d24dadcd800953c1e17fa782e7bd37894
manyagulati1604 <123478351+manyagulati1604@users.noreply.github.com>
2024-09-30 11:43:54 +0530

    Add files via upload

---
+++ src/currentLocation.js
@@ -0,0 +1,129 @@
+import React from "react";
+import apiKeys from "./apiKeys";
+import Forcast from "./forcast";
+import loader from "./images/WeatherIcons.gif";
+import ReactAnimatedWeather from "react-animated-weather";
+
+const defaults = {
+  color: "white",
+  size: 112,
+  animate: true,
+};
+
+class Weather extends React.Component {
+  state = {
+    lat: undefined,
+    lon: undefined,
+    icon: "CLEAR_DAY",
+    main: undefined,
+  };
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      this.getPosition()
+        .then((position) => {
+          this.getWeather(position.coords.latitude, position.coords.longitude);
+        })
+        .catch(() => {
+          this.getWeather(28.67, 77.22);
+          alert(
+            "You have disabled location service. Allow 'This APP' to access your location. Your current location will be used for calculating Real time weather."
+          );
+        });
+    } else {
+      alert("Geolocation not available");
+    }
+
+    this.timerID = setInterval(
+      () => this.getWeather(this.state.lat, this.state.lon),
+      600000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  getPosition = (options) => {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
+
+  getWeather = async (lat, lon) => {
+    const api_call = await fetch(
+      `${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKeys.key}`
+    );
+    const data = await api_call.json();
+    this.setState({
+      lat: lat,
+      lon: lon,
+      main: data.weather[0].main,
+    });
+    switch (this.state.main) {
+      case "Haze":
+        this.setState({ icon: "CLEAR_DAY" });
+        break;
+      case "Clouds":
+        this.setState({ icon: "CLOUDY" });
+        break;
+      case "Rain":
+        this.setState({ icon: "RAIN" });
+        break;
+      case "Snow":
+        this.setState({ icon: "SNOW" });
+        break;
+      case "Dust":
+        this.setState({ icon: "WIND" });
+        break;
+      case "Drizzle":
+        this.setState({ icon: "SLEET" });
+        break;
+      case "Fog":
+        this.setState({ icon: "FOG" });
+        break;
+      case "Smoke":
+        this.setState({ icon: "FOG" });
+        break;
+      case "Tornado":
+        this.setState({ icon: "WIND" });
+        break;
+      default:
+        this.setState({ icon: "CLEAR_DAY" });
+    }
+  };
+
+  render() {
+    if (this.state.icon) {
+      return (
+        <React.Fragment>
+          <div className="mb-icon">
+            <ReactAnimatedWeather
+              icon={this.state.icon}
+              color={defaults.color}
+              size={defaults.size}
+              animate={defaults.animate}
+            />
+            <p>{this.state.main}</p>
+          </div>
+          <Forcast icon={this.state.icon} weather={this.state.main} />
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <img src={loader} style={{ width: "50%", WebkitUserDrag: "none" }} />
+          <h3 style={{ color: "white", fontSize: "22px", fontWeight: "600" }}>
+            Detecting your location
+          </h3>
+          <h3 style={{ color: "white", marginTop: "10px" }}>
+            Your current location will be displayed on the App <br /> & used for
+            calculating Real time weather.
+          </h3>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+export default Weather;
